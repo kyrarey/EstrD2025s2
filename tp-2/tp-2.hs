7272bf935ccc1ae71e051dc6e534eff240c67b77
kyra rey <81933769+kyrarey@users.noreply.github.com>
2025-09-01 19:05:41 -0300

    p1 correcta

--- tp-2/tp-2.hs
+++ tp-2/tp-2.hs
@@ -19,22 +19,24 @@ sucesores (x : xs) = x + 1 : sucesores xs
 
 -- 4
 conjuncion :: [Bool] -> Bool
-conjuncion [] = True -- porque el neutro es true
+conjuncion [] = True
 conjuncion (x : xs) = x && conjuncion xs
 
 -- 5
 disyuncion :: [Bool] -> Bool
-disyuncion [] = True
+disyuncion [] = False
 disyuncion (x : xs) = x || disyuncion xs
 
 -- 6
+{-
 aplanar :: [[a]] -> [a]
-aplanar [[]] = []
-aplanar [(x : xs)] = x : aplanar xs
+aplanar [] = []
+aplanar (xs : xss) = xs : aplanar xss
+-}
 
 -- 7
 pertenece :: (Eq a) => a -> [a] -> Bool
-pertenece _ [] = True
+pertenece _ [] = False
 pertenece e (x : xs) =
   if x == e
     then True
@@ -52,47 +54,57 @@ apariciones e (x : xs) =
 losMenoresA :: Int -> [Int] -> [Int]
 losMenoresA _ [] = []
 losMenoresA n (x : xs) =
-  if n < x
+  if n > x
     then x : losMenoresA n xs
     else losMenoresA n xs
 
 -- 10
 lasDeLongitudMayorA :: Int -> [[a]] -> [[a]]
-lasDeLongitudMayorA _ [[]] = []
+lasDeLongitudMayorA _ [] = []
 lasDeLongitudMayorA n (x : xs) =
-  if longitud xs > n -- x es una lista y xs es una lista de listas
+  if longitud x > n -- x es una lista y xs es una lista de listas
     then x : lasDeLongitudMayorA n xs
     else lasDeLongitudMayorA n xs
 
 -- 11
 agregarAlFinal :: [a] -> a -> [a]
 agregarAlFinal [] e = [e]
-agregarAlFinal (x:xs) e = x:agregarAlFinal xs e
+agregarAlFinal (x : xs) e = x : agregarAlFinal xs e
 
--- 12 ! incompleto, subcaso recursivo?
+-- 12
 agregar :: [a] -> [a] -> [a]
-agregar [] [] = []
-agregar [a] [b] = a:b 
+agregar xs [] = xs
+agregar [] ys = ys
+agregar (x : xs) ys = x : agregar xs ys
 
 -- 13
+{-
 reversa :: [a] -> [a]
 reversa [] = []
-reversa (x:xs) = reversa xs:x
+reversa (x : xs) = reversa xs : x
+-}
 
 -- 14
 zipMaximos :: [Int] -> [Int] -> [Int]
 zipMaximos [] [] = []
-zipMaximos [a] [] = [a]
-zipMaximos [] [b] = [b]
-zipMaximos (a:as) (b:bs) = if a>b 
-                            then a:zipMaximos as bs
-                            else b:zipMaximos as bs
+zipMaximos xs [] = xs
+zipMaximos [] ys = ys
+zipMaximos (x : xs) (y : ys) =
+  if x > y
+    then x : zipMaximos xs ys
+    else y : zipMaximos xs ys
+
 -- 15
-elMinimo :: Ord a => [a] -> a --que es ord?
-elMinimo [] = -- precon: no debe ser una lista vacia
-elMinimo (x:xs) =  -- 
+elMinimo :: (Ord a) => [a] -> a -- que es ord?
+-- precon : no debe ser una lista vacia
+elMinimo [] = error "No debe darse una lista vacia"
+elMinimo [x] = x
+elMinimo (x : xs) =
+  if x < elMinimo xs
+    then x
+    else elMinimo xs
 
-
+{-
 -- 2 RECURSION SOBRE NUMEROS
 
 -- 1
@@ -102,4 +114,6 @@ factorial 0 = 1
 factorial n = (n * antecesor n) + factorial antecesor n
 
 antecesor :: Int -> Int
-antecesor n : n-1
+
+antecesor n : n - 1
+-}
