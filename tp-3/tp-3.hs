0c6c15538561b28c5f1d701162459c6caa892ae7
kyra rey <81933769+kyrarey@users.noreply.github.com>
2025-09-09 00:19:30 -0300

    p3 2.1.6

--- tp-3/tp-3.hs
+++ tp-3/tp-3.hs
@@ -44,7 +44,7 @@ ponerN n c (Bolita c2 cel) =
 
 -- 1.2 CAMINO HACIA EL TESORO
 data Objeto = Cacharro | Tesoro
-  deriving (Eq)
+  deriving (Show)
 
 data Camino = Fin | Cofre [Objeto] Camino | Nada Camino
   deriving (Show)
@@ -52,9 +52,7 @@ data Camino = Fin | Cofre [Objeto] Camino | Nada Camino
 -- 1.2.1
 hayTesoro :: Camino -> Bool
 hayTesoro Fin = False
-hayTesoro (Cofre objs c) = if hayTesoroEnObjs objs
-                              then True
-                              else hayTesoro c
+hayTesoro (Cofre objs c) = hayTesoroEnObjs objs || hayTesoro c
 hayTesoro (Nada c) = hayTesoro c
 
 hayTesoroEnObjs :: [Objeto] -> Bool
@@ -72,7 +70,7 @@ pasosHastaTesoro Fin = 0
 pasosHastaTesoro (Cofre objs c) = unoSi (not hayTesoroEnObjs objs) + pasosHastaTesoro c
 pasosHastaTesoro (Nada c) = pasosHastaTesoro c
 
--- 1.2.3 !
+-- 1.2.3
 hayTesoroEn :: Int -> Camino -> Bool
 hayTesoroEn _ Fin = 0
 hayTesoroEn n (Cofre objs c) = if pasosHastaTesoro c /= n
@@ -83,5 +81,60 @@ hayTesoroEn _ (Nada c) = hayTesoroEn c
 -- 1.2.4
 alMenosNTesoros :: Int -> Camino -> Bool
 alMenosNTesoros _ Fin = False
---alMenosNTesoros n (Cofre objs c) = n objs alMenosNTesoros c
+alMenosNTesoros n (Cofre objs c) = (numTotalDeTesoros objs c) > n || alMenosNTesoros c
 alMenosNTesoros _ (Nada c) = alMenosNTesoros c
+
+numTotalDeTesoros :: [Objeto] -> Camino -> Int
+numTotalDeTesoros _ Fin = 0
+numTotalDeTesoros objs (Cofre objs1 c) = unoSi (hayTesoroEnObjs objs) + numTotalDeTesoros c
+numTotalDeTesoros _ (Nada c) = numTotalDeTesoros c
+
+-- 1.2.5
+cantTesorosEntre :: Int -> Int -> Camino -> Int
+cantTesorosEntre _ _ Fin = 0
+cantTesorosEntre n1 n2 (Cofre objs c) = if numPasosEnCamino n1 >= 3 || numPasosEnCamino n2 >= 5
+                                          then unoSi (hayTesoroEnObjs objs) + cantTesorosEntre c
+                                          else cantTesorosEntre c
+cantTesorosEntre _ _ (Nada c) = cantTesorosEntre c
+
+
+-- 2 TIPOS ARBOREOS
+
+-- 2.1. ARBOLES BINARIOS
+data Tree a = EmptyT | NodeT a (Tree a) (Tree a)
+  deriving (Show)
+
+-- 2.1.1
+sumarT :: Tree Int -> Int
+sumarT EmptyT = 0
+sumarT (NodeT x t1 t2) = x + sumarT t1 + sumarT t2 
+
+-- 2.1.2
+sizeT :: Tree a -> Int
+sumarT EmptyT = 0
+sumarT (NodeT x t1 t2) = lenght x + sizeT t1 + sizeT t2 
+
+-- 2.1.3
+mapDobleT :: Tree Int -> Tree Int
+mapDobleT EmptyT = EmptyT
+mapDobleT (NodeT x t1 t2) = Tree (NodeT x*2 (mapDobleT t1) (mapDobleT t2) )
+
+-- 2.1.4
+perteneceT :: Eq a => a -> Tree a -> Bool
+perteneceT _ EmptyT = EmptyT
+perteneceT e (NodeT x t1 t2) = e==x || perteneceT t1 || perteneceT t2
+
+-- 2.1.5
+aparicionesT :: Eq a => a -> Tree a -> Int
+aparicionesT _ EmptyT = EmptyT
+aparicionesT e (NodeT x t1 t2) = unoSi (e == x) + aparicionesT t1 + aparicionesT t2
+
+-- 2.1.6
+leaves :: Tree a -> [a]
+leaves EmptyT = EmptyT
+leaves (NodeT x t1 t2) = siEsHoja (NodeT x t1 t2) ++ leaves t1 ++ leaves t2
+
+siEsHoja :: Tree a -> [a]
+siEsHoja (NodeT x EmptyT EmptyT) = [x]
+siEsHoja _ = [False]
+
