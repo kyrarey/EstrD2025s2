18535c241c8cd783cafffc4f58b1dd2f5b3b047f
kyra rey <81933769+kyrarey@users.noreply.github.com>
2025-09-09 23:17:02 -0300

    p3 entrega

--- tp-3/tp-3.hs
+++ tp-3/tp-3.hs
@@ -23,8 +23,8 @@ unoSi True = 1
 unoSi False = 0
 
 -- 1.1.2
-poner :: Color -> Celda -> Celda
-poner c1 (Bolita c2 cel) = Bolita c1 (Bolita c2 cel)
+poner :: Color -> Celda -> Celda 
+poner c b = Bolita c b
 
 -- 1.1.3
 sacar :: Color -> Celda -> Celda
@@ -36,8 +36,8 @@ sacar c1 (Bolita c2 cel) =
 
 -- 1.1.4
 ponerN :: Int -> Color -> Celda -> Celda
-ponerN 0 _ cel = cel
-ponerN n c (Bolita c2 cel) = ponerN (n - 1) c (poner c cel)
+ponerN 0 _ b = b
+ponerN n c b = Bolita c (ponerN (n-1) c b)
 
 -- 1.2 CAMINO HACIA EL TESORO
 data Objeto = Cacharro | Tesoro
@@ -47,27 +47,27 @@ data Camino = Fin | Cofre [Objeto] Camino | Nada Camino
   deriving (Show)
 
 --TEST
-c1 = Cofre [Cacharro, Tesoro] 
-        (Nada 
-            (Cofre [Cacharro, Cacharro] Fin)
-            )
-c2 = Nada ( 
-        Cofre [Cacharro, Cacharro] 
-            (Nada 
-                (Cofre [Cacharro, Cacharro] Fin))
-          )
+-- c1 = Cofre [Cacharro, Tesoro] 
+--         (Nada 
+--             (Cofre [Cacharro, Cacharro] Fin)
+--             )
+-- c2 = Nada ( 
+--         Cofre [Cacharro, Cacharro] 
+--             (Nada 
+--                 (Cofre [Cacharro, Cacharro] Fin))
+--           )
 
-c3 = Nada (
-          Cofre [Tesoro, Cacharro] 
-              (Nada 
-                  (Cofre [Tesoro, Tesoro] Fin))
-          )
+-- c3 = Nada (
+--           Cofre [Tesoro, Cacharro] 
+--               (Nada 
+--                   (Cofre [Tesoro, Tesoro] Fin))
+--           )
 
-c4 = Cofre [Cacharro] 
-        (Nada 
-            (Cofre [Cacharro, Cacharro] 
-                (Cofre [Cacharro, Tesoro] Fin))
-        )
+-- c4 = Cofre [Cacharro] 
+--         (Nada 
+--             (Cofre [Cacharro, Cacharro] 
+--                 (Cofre [Cacharro, Tesoro] Fin))
+--         )
 
 
 -- 1.2.1
@@ -95,11 +95,10 @@ pasosHastaTesoro (Nada c) = 1 + pasosHastaTesoro c
 
 -- 1.2.3
 hayTesoroEn :: Int -> Camino -> Bool
-hayTesoroEn _ Fin = False
-hayTesoroEn n (Cofre objs c) = if pasosHastaTesoro c /= n
-                                  then hayTesoroEn (n-1) c
-                                  else hayTesoroEnObjs objs 
-hayTesoroEn n (Nada c) = hayTesoroEn n c
+hayTesoroEn 0 (Cofre objs c) = hayTesoroEnObjs objs
+hayTesoroEn n Fin          = False
+hayTesoroEn n (Cofre objs c) = hayTesoroEn (n-1) c
+hayTesoroEn n (Nada c)     = hayTesoroEn (n-1) c
 
 -- 1.2.4
 alMenosNTesoros :: Int -> Camino -> Bool
@@ -116,20 +115,21 @@ cantTesorosEnObjs (obj:objs) = unoSi (esTesoro obj) + cantTesorosEnObjs objs
 
 -- 1.2.5 !!
 cantTesorosEntre :: Int -> Int -> Camino -> Int
+-- precondicion: n1 no puede ser mayor a n2
 cantTesorosEntre _ _ Fin = 0
-cantTesorosEntre n1 n2 (Cofre objs c) = if numPasosEnCamino c >= n1 || numPasosEnCamino c <= n2
-                                          then cantTesorosEnObjs objs + cantTesorosEntre n1 n2 c
-                                          else cantTesorosEntre n1 n2 c
-cantTesorosEntre n1 n2 (Nada c) = cantTesorosEntre n1 n2 c
+cantTesorosEntre 0 0 c = cantTesorosSiHay c
+cantTesorosEntre 0 n2 c = cantTesorosSiHay c + cantTesorosEntre 0 (n2-1) (darUnPaso c)
+cantTesorosEntre n1 n2 (Nada c) = cantTesorosEntre (n1-1) (n2-1) (darUnPaso c)
 
-numPasosEnCamino :: Camino -> Int
-numPasosEnCamino Fin = 0
-numPasosEnCamino (Cofre objs c) = unoSi (noEsFinal c) + numPasosEnCamino c
-numPasosEnCamino (Nada c) = numPasosEnCamino c
+cantTesorosSiHay :: Camino -> Int
+cantTesorosSiHay (Cofre objs _ ) = cantTesorosEnObjs objs
+cantTesorosSiHay _ = 0
 
-noEsFinal :: Camino -> Bool
-noEsFinal Fin = False
-noEsFinal _ = True
+darUnPaso :: Camino -> Camino
+-- precondicion = Debe haber un camino siguiente
+darUnPaso (Cofre _ c ) = c
+darUnPaso (Nada c) = c
+darUnPaso Fin = error "No hay mas camino"
 
 -- 2 TIPOS ARBOREOS
 
@@ -137,28 +137,28 @@ noEsFinal _ = True
 data Tree a = EmptyT | NodeT a (Tree a) (Tree a)
   deriving (Show)
 
-t1 :: Tree Int
-t1 = NodeT 11 (NodeT 22 EmptyT EmptyT) (NodeT 37 EmptyT EmptyT)
-t2 :: Tree Int
-t2 = NodeT 1 (NodeT 1 EmptyT EmptyT) (NodeT 2 EmptyT EmptyT)
-t3 :: Tree Int
-t3 = NodeT 12 (NodeT 11 EmptyT 
-                  (NodeT 45 EmptyT EmptyT)
-              )
-              (NodeT 2 EmptyT
-                  (NodeT 23 EmptyT
-                      (NodeT 45 EmptyT EmptyT))
-              )
-t4 :: Tree Int
-t4 = NodeT 56 (NodeT 8 EmptyT 
-                  (NodeT 290 EmptyT
-                      (NodeT 76 EmptyT
-                        (NodeT 46 EmptyT EmptyT)))
-              )
-              (NodeT 72 EmptyT
-                  (NodeT 509 EmptyT
-                      (NodeT 98 EmptyT EmptyT))
-              )
+-- t1 :: Tree Int
+-- t1 = NodeT 11 (NodeT 22 EmptyT EmptyT) (NodeT 37 EmptyT EmptyT)
+-- t2 :: Tree Int
+-- t2 = NodeT 1 (NodeT 1 EmptyT EmptyT) (NodeT 2 EmptyT EmptyT)
+-- t3 :: Tree Int
+-- t3 = NodeT 12 (NodeT 11 EmptyT 
+--                   (NodeT 45 EmptyT EmptyT)
+--               )
+--               (NodeT 2 EmptyT
+--                   (NodeT 23 EmptyT
+--                       (NodeT 45 EmptyT EmptyT))
+--               )
+-- t4 :: Tree Int
+-- t4 = NodeT 56 (NodeT 8 EmptyT 
+--                   (NodeT 290 EmptyT
+--                       (NodeT 76 EmptyT
+--                         (NodeT 46 EmptyT EmptyT)))
+--               )
+--               (NodeT 72 EmptyT
+--                   (NodeT 509 EmptyT
+--                       (NodeT 98 EmptyT EmptyT))
+--               )
 
 -- 2.1.1
 sumarT :: Tree Int -> Int
@@ -197,9 +197,12 @@ siEsHoja _ = []
 -- 2.1.7
 heightT :: Tree a -> Int
 heightT EmptyT = 0
-heightT (NodeT x t1 t2) = if sizeT t1 > sizeT t2
-                              then unoSi (not (esHoja t1)) + heightT t1
-                              else unoSi (not (esHoja t2)) + heightT t2
+heightT (NodeT x t1 t2) = 1 + elMasGrande (heightT t1) (heightT t2)
+
+elMasGrande :: Int -> Int -> Int
+elMasGrande n n1 = if n < n1
+                    then n1
+                    else n
                 
 esHoja :: Tree a -> Bool
 esHoja EmptyT = True
@@ -222,23 +225,56 @@ levelN _ EmptyT = []
 levelN n (NodeT x t1 t2) = levelN (n-1) t1 ++ levelN (n-1) t2
 
 -- 2.1.11
--- listPerLevel :: Tree a -> [[a]]  
--- listPerLevel EmptyT =
--- listPerLevel (NodeT x t1 t2) = x listPerLevel t1 listPerLevel t2 
+listPerLevel :: Tree a -> [[a]]
+listPerLevel EmptyT = []
+listPerLevel (NodeT x t1 t2) = [x] : juntarNiveles (listPerLevel t1) (listPerLevel t2)
+
+juntarNiveles :: [[a]] -> [[a]] -> [[a]]
+juntarNiveles xs [] = xs
+juntarNiveles [] ys = ys
+juntarNiveles (xs:xss) (ys:yss) = (xs ++ ys) : juntarNiveles xss yss
 
 -- 2.1.12
 ramaMasLarga :: Tree a -> [a]
 ramaMasLarga EmptyT = []
-ramaMasLarga (NodeT x t1 t2) = if sizeT t1> sizeT t2
-                                  then x:ramaMasLarga t1
-                                  else x:ramaMasLarga t2 --si son del mismo largo devuelve la rama derecha
+ramaMasLarga (NodeT x t1 t2) = x : laMasLarga (ramaMasLarga t1) (ramaMasLarga t2)
+
+laMasLarga :: [a] -> [a] -> [a]
+laMasLarga x y = if length x > length y
+                  then x
+                  else y
 
 -- 2.1.13
--- todosLosCaminos :: Tree a -> [[a]]
--- todosLosCaminos EmptyT = []
--- todosLosCaminos (NodeT x t1 t2) = 
---   [x: maximal t1] ++ todosLosCaminos t1 ++ [x: maximal t2] ++ todosLosCaminos t2
+todosLosCaminos :: Tree a -> [[a]]
+todosLosCaminos EmptyT = []
+todosLosCaminos (NodeT x t1 t2) = [x] : (consA x (todosLosCaminos t1)) ++ (consA x (todosLosCaminos t2))
 
--- maximal :: Tree a -> [a]
--- maximal EmptyT = []
--- maximal (NodeT x t1 t2) = [x]
+consA :: a -> [[a]] -> [[a]]
+consA _ [] = []
+consA y (xs:xss) = (y: xs) : (consA y xss)
+
+
+--2.2 EXPRESIONES ARITMETICAS
+data ExpA = Valor Int | Sum ExpA ExpA | Prod ExpA ExpA | Neg ExpA
+   deriving Show
+
+-- 2.2.1
+eval :: ExpA -> Int
+eval (Valor n)    = n
+eval (Sum n1 n2)  = eval n1 + eval n2
+eval (Prod n1 n2) = eval n1 * eval n2
+eval (Neg n) = -(eval n)
+
+-- 2.2.2
+simplificar :: ExpA -> ExpA
+simplificar (Valor n)          = Valor n
+simplificar (Sum (Valor 0) n)  = simplificar n
+simplificar (Sum n (Valor 0))  = simplificar n
+simplificar (Prod (Valor 0) n) = Valor 0
+simplificar (Prod n (Valor 0)) = Valor 0
+simplificar (Prod (Valor 1) n) = simplificar n
+simplificar (Prod n (Valor 1)) = simplificar n
+simplificar (Neg (Neg n))      = simplificar n
+simplificar (Sum n1 n2)        = (Sum (simplificar n1) (simplificar n2))
+simplificar (Prod n1 n2)       = (Prod (simplificar n1) (simplificar n2))
+simplificar (Neg n)            = (Neg (simplificar n))
